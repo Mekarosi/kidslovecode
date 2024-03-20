4302b91e35fd85a113b59c35f99d76e78495ce5c
Mekarosi <mekarosi_assa@yahoo.com>
2024-03-20 10:30:34 -0700

    Added 3d models

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
+import Robot from '../models/Robot.jsx'
+
+
  
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
   POPUP
@@ -20,6 +23,8 @@ const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight />
+
+          <Robot />
         </Suspense>
       </Canvas>
     </section>
