8e0f64d3a532d23440c0c7ef080b10c2e66e0209
Mekarosi <mekarosi_assa@yahoo.com>
2024-03-23 11:07:23 -0700

    Added events to make models move and rotate

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,14 +1,21 @@
-import { useGLTF } from '@react-three/drei'
-import React from 'react'
+import { useAnimations, useGLTF } from '@react-three/drei'
+import React, { useRef, useEffect } from 'react'
 
 import birdScene from '../assets/3d/bird.glb'
 
 const Bird = () => {
     const { scene, animations} = useGLTF(birdScene)
-  return (
+    const birdRef = useRef()
+    const { actions } = useAnimations(animations,birdRef)
+  
+    // useEffect((_, delta) => {
+    //   actions['Take 001'].play()
+    //  },[])
+
+    return (
     <mesh position={[-5, 2, 1]} scale={[0.0015, 0.0015, 0.0015]}>
-    <primitive object={scene} />
-   </mesh>
+      <primitive object={scene} />
+    </mesh>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
@@ -12,8 +12,9 @@ import Plane from '../models/Plane'
 </div> */}
 
 const Home = () => {
-
-  //  const adjustOfficeForScreenSize  =  () => {
+  const [isRotating, setIsRotating] = useState(false)
+  const [currentStage, setCurrentStage] = useState(1) 
+  //  const adjustRobotForScreenSize  =  () => {
   //    let screenScale = null
   //    let screenPosition = [0, -6.5, -43]
   //    let rotation = [0.1, 4.7, 0]
@@ -28,10 +29,31 @@ const Home = () => {
   // }
 
   // const [robotScale, robotPosition, robotRotation] = adjustOfficeForScreenSize()
+ 
+
+    const adjustPlaneForScreenSize  =  () => {
+     let screenScale, screenPosition 
+
+     if(window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5]
+      screenPosition = [0, -1.5, 0]
+     } else {
+      screenScale = [3, 3, 3]
+      screenPosition = [0, -4, -4]
+     }
+
+     return [screenScale, screenPosition]
+  }
+
+  const [planeScale, planePosition] = adjustPlaneForScreenSize()
+ 
+ 
+ 
+ 
   return (
     <section className='w-full h-screen relative'>
       <Canvas 
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -45,8 +67,17 @@ const Home = () => {
             // position={robotPosition}
             // scale={robotScale}
             // rotation={robotRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
           />
-          <Plane />
+          <Plane 
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            planeScale={planeScale} 
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>
