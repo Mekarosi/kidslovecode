9949decf136bc07cb282db4acd567c2ee7164407
Mekarosi <mekarosi_assa@yahoo.com>
2024-04-14 10:27:33 -0700

    Adjusted 3d humanoid model to fit screen size

--- src/pages/SchoolTutoring.jsx
+++ src/pages/SchoolTutoring.jsx
@@ -3,12 +3,32 @@ import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
 import  Game  from '../models/Game'
+import  Humanoid  from '../models/Humanoid'
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
   POPUP
 </div> */}
 
+
 const SchoolTutoring = () => {
+
+  const adjustHumanoidForScreenSize = () => {
+    let screenScale = null
+    let screenPosition = [25.5, -17, -43]
+    let rotation = [0.1, -0.7, 0]
+
+    if(window.innerWidth < 768){
+      screenScale = [3, 3, 3]
+      screenPosition 
+    } else {
+      screenScale = [3, 3, 3]
+      
+    }
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [humanoidScale, humanoidPosition , humanoidRotation] = adjustHumanoidForScreenSize()
+
   return (
     <section className='w-full h-screen relative'>
       <Canvas 
@@ -22,6 +42,11 @@ const SchoolTutoring = () => {
           <spotLight />
           <hemisphereLight />
 
+          <Humanoid 
+            position={humanoidPosition}
+            scale={humanoidScale}
+            rotation={humanoidRotation}
+          />
           <Game /> 
         </Suspense>
       </Canvas>
